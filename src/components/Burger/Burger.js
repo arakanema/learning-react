fda73198f8f6f074a1b2e2c741162395d4b1885e
Masahiro Arakane <arakane.masahiro@gmail.com>
2018-01-03 23:48:44 +0900

    112. Starting the Burger Component Burger.jsを実装し、BurgerBuilderに渡すところ

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) => {
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type={'bread-top'}/>
+      {/*<BurgerIngredient type={props.type}/>*/}
+      <BurgerIngredient type={'meat'}/>
+      <BurgerIngredient type={'cheese'}/>
+      <BurgerIngredient type={'salad'}/>
+      <BurgerIngredient type={'bread-bottom'}/>
+    </div>
+  );
+};
+
+export default burger;
+
